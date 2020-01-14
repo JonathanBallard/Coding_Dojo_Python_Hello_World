bc0f00d4b7ced7b58581449b85db22347f5b54c5
Jonathan Ballard <asgardschosen@gmail.com>
2020-01-14 11:41:12 -0800

    updated assignment problem 3 (optional)

--- hello_world.py
+++ hello_world.py
@@ -7,13 +7,12 @@ print( "hello " + name )	# with a +
 # 3. print "Hello 42!" with the number in a variable
 name = 2
 print( "hello", name )	# with a comma
-print( "Hello {}".format(name) )	# with a +	-- this one should give us an error!
+print( "Hello" + str(name) )	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
 print( "I love to eat {} and {}".format(fave_food1, fave_food2) ) # with .format()
 print( f"I love to eat {fave_food1} and {fave_food2}" ) # with an f string
-
 
 print( "hello".upper(), name ) #uppercase
 print( "HELLO".lower(), name ) #lowercase
@@ -21,7 +20,6 @@ print( "HELLO".lower(), name ) #lowercase
 
 h = "hello".find("l")
 print("first l is at index", h)
-
 
 
 
